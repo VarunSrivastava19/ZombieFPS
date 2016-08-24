f5f44e3788ebfa224ca12da8098ad91df181b3b5
Nicholas Primiano <njp280@nyu.edu>
2016-08-23 22:31:33 -0400

    no message

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eyes : MonoBehaviour
+{
+	private Camera eyes;
+	private float defaultFOV;
+
+	void Start ()
+	{
+		eyes = GetComponent<Camera> ();
+	}
+
+	void Update ()
+	{
+	
+	}
+}
