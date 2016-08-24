b73eded316e0578a97ee560987c95bde732a8f9d
Nicholas Primiano <njp280@nyu.edu>
2016-08-23 22:29:06 -0400

    Finish player respawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,20 +8,27 @@ public class Player : MonoBehaviour
 	public bool reSpawn = false;
 
 	private Transform[] spawnPoints;
+	private bool lastToggle = false;
 
 	void Start ()
 	{
-		spawnPoints = GetComponentInChildren<Transform> ();
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
 	}
 
 	void Update ()
 	{
-	
+		if (lastToggle != reSpawn) {
+			Respawn ();
+			reSpawn = false;
+		} else {
+			lastToggle = reSpawn;
+		}
 	}
 
 	private void Respawn ()
 	{
 		int i = Random.Range (1, spawnPoints.Length);
+		transform.position = spawnPoints [i].transform.position;
 	}
 
 }
