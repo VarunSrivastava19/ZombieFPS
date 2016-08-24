d75f0d8c9e30430a1c4035183e294805182ae92c
Nicholas Primiano <njp280@nyu.edu>
2016-08-23 22:17:19 -0400

    Begin player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public Transform playerSpawnPoints;
+	// The parent of the spawn point
+	public bool reSpawn = false;
+
+	private Transform[] spawnPoints;
+
+	void Start ()
+	{
+		spawnPoints = GetComponentInChildren<Transform> ();
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	private void Respawn ()
+	{
+		int i = Random.Range (1, spawnPoints.Length);
+	}
+
+}
