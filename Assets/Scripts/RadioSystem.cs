604685f6374b00acbd80516ef8d0796b96d63da8
Nicholas Primiano <njp280@nyu.edu>
2016-08-24 21:32:57 -0400

    Better audio

--- Assets/Scripts/RadioSystem.cs
+++ Assets/Scripts/RadioSystem.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RadioSystem : MonoBehaviour {
+public class RadioSystem : MonoBehaviour
+{
 
+	public AudioClip initialHeliCall;
+	public AudioClip initialCallReply;
+
+	private AudioSource audioSource;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void OnMakeInitialHeliCall ()
+	{
+		print (name + "OnMakeInitialHeliCall");
+		audioSource.clip = initialHeliCall;
+		audioSource.Play ();
+		Invoke ("InitialReply", initialHeliCall.length + 1f);
 	}
+
+	void InitalReply ()
+	{
+		audioSource.clip = initialCallReply;
+		audioSource.Play ();
+		BroadcastMessage ("OnDispachHelicopter");
+	}
+		
 }
